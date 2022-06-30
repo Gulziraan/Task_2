892fafa7c9dd7f1657df595050ae16c99827fe88
gulzira <gulzira.anarbarva@gmail.com>
2022-06-30 11:51:57 +0300

    Add project

---
+++ ConfigEditorForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace taskk2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToOrderColumns = true;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView2.Location = new System.Drawing.Point(0, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(799, 265);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Лаборатория";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 400;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Строка подключения";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 400;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(182, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Поле \"лаборатория\" должно быть заполнено!";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "*";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConfigEditorForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace taskk2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool b;
+            if (File.Exists("connectionConfigs.json"))
+                b = true;
+            else
+            { return; }
+            string json = File.ReadAllText("connectionConfigs.json");
+            var temp = (Root)JsonConvert.DeserializeObject(json, typeof(Root));
+
+            foreach (Member item in temp.members)
+            {
+                string[] lines = new string[] { item.LaboratoryName, item.ConnectionString };
+                dataGridView2.Rows.Add(lines[0], lines[1]);
+            }
+        }
+
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool a = true;
+            for (int i = 0; i < dataGridView2.Rows.Count - 1; i++)
+            {
+                    if (dataGridView2[0,i].Value==null)
+                    {
+                        label1.Visible = true;
+                        label2.Visible = true;
+                    return;
+                    }
+                }
+                label1.Visible = false;
+                label2.Visible = false;
+            Member[] Datavalue = new Member[dataGridView2.Rows.Count - 1];
+                for(int i = 0;i < dataGridView2.Rows.Count - 1; i++)
+                {
+                if (dataGridView2[1, i].Value != null) 
+                    Datavalue[i] = new Member(dataGridView2[0, i].Value.ToString(), dataGridView2[1, i].Value.ToString());
+                else Datavalue[i] = new Member(dataGridView2[0, i].Value.ToString(), " ");
+
+            }
+                string json = "{\"members\":" + JsonConvert.SerializeObject(Datavalue) + "}";
+            if (File.Exists("connectionConfigs.json"))
+                File.WriteAllText("connectionConfigs.json", json);
+            else { File.Create("connectionConfigs.json").Close(); File.WriteAllText("connectionConfigs.json", json); }
+            }
+    }
+            
+    public class Member
+    {
+        public Member(string laboratoryName, string connectionString)
+        {
+            LaboratoryName = laboratoryName;
+            ConnectionString = connectionString;
+        }
+
+        public string LaboratoryName { get; set; }
+        public string ConnectionString { get; set; }
+        public override string ToString()
+        {
+            return LaboratoryName + " " + ConnectionString;
+        }
+    }
+
+    public class Root
+    {
+        public List<Member>  members { get; set; }
+    }
+
+}
